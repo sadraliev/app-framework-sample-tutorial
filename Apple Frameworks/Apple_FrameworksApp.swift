d7e633b59833b89a67eb2ed50cce6ad242b7fb63
sadraliev <a.sadraliev@gmail.com>
2024-08-24 19:59:41 -0400

    Initial Commit

---
+++ Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Aibek Sadraliev on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
